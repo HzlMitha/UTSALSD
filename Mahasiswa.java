35aec3026b929070e8247db52f9279ead3aa4d7d
Dian Paramitha <dianmitha89031@gmail.com>
2025-04-16 13:30:13 +0700

    data class Mhs

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,5 +1,25 @@
 public class Mahasiswa {
+    String nim;
+    String nama;
+    String prodi; 
+    
     public Mahasiswa () {
-        
+    
     }
-}
+    
+    public Mahasiswa (String nim, String nama, String prodi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+    
+    void tampilMahasiswa() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("NIM/t : " + nim);
+            System.out.println("Nama/t : " + nama);
+            System.out.println("Prodi/t : " + prodi);
+        }
+    }
+}    
+
+
