eca44b564d5923b93d0269119637cfd96f92c0ce
farras55 <farraskuyy@gmail.com>
2025-04-16 11:59:54 +0700

    TES

--- TES.java
+++ TES.java
@@ -5,5 +5,6 @@ public class TES {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, world!");
+        System.out.println("Hello, world!"); // This is a comment
     }
 }
