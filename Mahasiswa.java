17e4c2f7c77b80dfe5d9ba2e1d901d3b473257e3
farras55 <farraskuyy@gmail.com>
2025-04-16 12:12:29 +0700

    TES

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,5 +1,10 @@
 package UTSALSD;
 
 public class Mahasiswa {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, world!");
+        System.out.println("Hello, world!"); // This is a comment
+        System.out.println("Hello, world!"); // This is a comment
+    }
 }
