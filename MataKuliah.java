75024a92523a88585007c20e3d95044657544f30
Dian Paramitha <dianmitha89031@gmail.com>
2025-04-16 14:30:47 +0700

    penambahan class Matkul dan Main

---
+++ MataKuliah.java
@@ -0,0 +1,22 @@
+public class MataKuliah {
+    String kodeMk;
+    String namaMK;
+    int sks;
+
+    public MataKuliah () {
+
+    }
+
+    public MataKuliah (String kdMatkul, String nmMatkul, int sks) {
+        kodeMk = kdMatkul;
+        namaMK = nmMatkul;
+        this.sks = sks;
+    }
+
+    void tampilMataKuliah () {
+        System.out.println("Daftar Mata Kuliah:");
+        for (int i = 0; i < 5; i++) {
+            
+        }
+    }
+}
---
+++ SistemPengelolaan.java
@@ -0,0 +1,6 @@
+public class SistemPengelolaan {
+    
+    public static void main(String[] args) {
+        
+    }
+}
