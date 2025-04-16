9de2fd06d853908713f2aa4b199a0b63410d1ef6
nesslty <pineapplekuu@gmail.com>
2025-04-16 15:17:11 +0700

    parameter

--- Penilaian.java
+++ Penilaian.java
@@ -1,10 +1,17 @@
 public class Penilaian {
+    Mahasiswa mahasiswa;
+    MataKuliah mataKuliah;
     double nilaiTugas;
     double nilaiUTS;
     double nilaiUAS;
     double nilaiAkhir;
-    
-    public Penilaian () {
 
+    public Penilaian (Mahasiswa mhs, MataKuliah mk, double tugas, double uts, double uas) {
+        mahasiswa = mhs;
+        mataKuliah = mk;
+        nilaiTugas = tugas;
+        nilaiUTS = uts;
+        nilaiUAS = uas;
     }
+
 }
